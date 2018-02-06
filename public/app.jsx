0145b0ad1a1816130e1c000bceb82c2928260e11
Felipe Alfonso González <f.alfonso.go@gmail.com>
2018-02-06 04:03:52 -0300

    v2.1.0.2rvc1.0

--- public/app.jsx
+++ public/app.jsx
@@ -1,8 +1,8 @@
 var Greeter = React.createClass({
     getDefaultProps: function () {
       return {
-        name: 'Theatherflix',
-        message: 'Theatherflix OpenSource Project - A visual tool for wiring the Internet of Things, digital movies, stored in your HD. The cousin of Netflix in the future.'
+        name: 'Theatherflix OpenSource Project',
+        message: 'A visual tool for wiring the Internet of Things, digital movies, stored in your HD. The cousin of Netflix in the future.'
       };
     },
     render: function () {
@@ -11,7 +11,8 @@ var Greeter = React.createClass({
   
       return (
         <div>
-          <h1>Hello {name}!</h1>
+          <h1>{name}</h1>
+
           <p>{message}</p>
         </div>
       );
@@ -20,7 +21,14 @@ var Greeter = React.createClass({
   
   
   ReactDOM.render(
-    <Greeter name={name}/>,
-    document.getElementById('app')
+    <Greeter />,
+    document.getElementById('theatherflixapp')
   );
   
+
+  // probably the logic will be like to continue with different containers to load the movies ....  but here's the thing! how to sync it without to code container after container ! .....
+  // cause with handlebars I had the same issue.... I gotta research ... in the end React is huge...  quite flexible ... 
+
+  // if you wanna code with good music in the background, I recommend : https://ibizasonica.com/
+
+
