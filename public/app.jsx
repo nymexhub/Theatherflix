a4f191a21e5edfc139fd7fa6515b344ddd63bd60
Felipe Alfonso González <f.alfonso.go@gmail.com>
2018-03-08 19:10:27 -0300

    RC 00221  - Applying React to the project... some errors ...

--- public/app.jsx
+++ public/app.jsx
@@ -11,7 +11,7 @@ var React = require('react');
 var ReactDOM = require('react-dom'); 
 
 
-var createReactClass = require('create-react-class');
+/// var createReactClass = require('create-react-class');
 
 // var createReactClass = require('create-react-class');
 
