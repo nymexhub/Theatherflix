5693de5f538303c4da2e78008f62987033754e73
Douglas Alves <douglas.expalves@gmail.com>
2018-03-09 23:01:14 -0300

    refactored app.jsx

--- public/app.jsx
+++ public/app.jsx
@@ -1,51 +1,16 @@
-
-// Coding by Felipe Alfonso Gonzalez -  Software Engineer/Developer  CC 2018 
+// Coding by Felipe Alfonso Gonzalez -  Software Engineer/Developer  CC 2018
 // ---------------------------------------------------------------
 // if you wanna code with good music in the background, I recommend : https://ibizasonica.com/
-
 // for future adjustaments from other coders please refactoring is necessary.
 
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+import Greeter from './greeter'
 
-var React = require('react');
-var ReactDOM = require('react-dom'); 
+// var to make a welcome ... from me
+const title = 'Felipe the Founder welcomes you!'
+const message = 'Our first Component'
 
-
-
-var Greeter = React.createClass({
-  getDefaultProps: function () {
-    return {
-      title: 'Theatherflix OpenSource Project',
-      message: 'A visual tool for wiring the Internet of Things, digital movies, stored in your HD. "The cousin of Netflix in the future".'
-    };
-  },
-  getInitialState: function () {
-    return {
-        title: this.props.title,
-        message: this.props.message
-    };
-  },
-  handleNewData: function (updates) {
-    this.setState(updates);
-  },
-  render: function () {
-    var title = this.state.title;
-    var message = this.state.message;
-
-    return (
-      <div>
-        <Greeter title={title} message={message}/>
-      </div>
-    );
-  }
-});
-
-// var to make a welcome ... from me 
-var firstName = 'Felipe the Founder welcomes you!';
-
-// the render 
-ReactDOM.render(
-    <Greeter name={firstName}/>,
-    document.getElementById('app')
-);
-
+// the renderer
+ReactDOM.render(<Greeter title={title} message={message} />, document.getElementById('app'))
