7ce25d8160404aa68807c29ae32194a5e5270147
Felipe Alfonso González <f.alfonso.go@gmail.com>
2018-04-04 03:29:13 -0300

    adding up some more little things... step by step

--- server.js
+++ server.js
@@ -45,16 +45,16 @@ app.use(express.static('public'));
 
 
 // in case the port is blocked or something.
-const find = require('find-process')
+// const find = require('find-process')
 
-const PORT = 3000
+// const PORT = 3000
 
-find('port', PORT)
-.then((list) => {
-  console.log(`Port "${PORT}" is blocked. Killing blocking applications...`)
-  const processIds = list.map((item) => item.pid)
-  processIds.forEach((pid) => process.kill(pid, 10))
-})
+// find('port', PORT)
+// .then((list) => {
+//   console.log(`Port "${PORT}" is blocked. Killing blocking applications...`)
+//   const processIds = list.map((item) => item.pid)
+//   processIds.forEach((pid) => process.kill(pid, 10))
+// })
 // end 
 
 
