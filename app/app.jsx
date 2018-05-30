138f893e4e6fea08634ccc0a293c84a780f81d82
Felipe <f.alfonso@res-ear.ch>
2018-05-29 23:26:26 -0400

    Boing 747. RVC 3.4.5

--- app/app.jsx
+++ app/app.jsx
@@ -9,6 +9,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+
 // Load foundation
 // $(document).foundation();
 
