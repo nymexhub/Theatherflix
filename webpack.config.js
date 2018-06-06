50520698ebb729dda3594766fe7635c3ab6e87a0
Felipe <f.alfonso@res-ear.ch>
2018-06-06 01:27:31 -0400

    Boing 747. yet rvc2.3.4

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,25 @@
-module.exports = {
-  entry: './app/app.jsx',
+module.exports= {
+  entry:'./app/app.jsx',
   output: {
     path: __dirname,
-    filename: './public/bundle.js'
+    filename:'./public/bundle.js'
   },
   resolve: {
-    root: __dirname,
-    alias: {
+    modules: [__dirname, 'node_modules'],
+    alias:{
       Main: 'app/components/Main.jsx',
       Nav: 'app/components/Nav.jsx'
     },
-    extensions: ['', '.js', '.jsx']
+    extensions: ['*','.js','.jsx']
   },
-  module: {
-    loaders: [
-      {
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0']
-        },
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/
-      }
-    ]
-  }
-};
-// please don't change this! inform first in the group!
-module.exports = {
-  entry: './app/app.jsx',
-  output: {
-    path: __dirname,
-    filename: './public/bundle.js'
-  },
-  resolve: {
-    root: __dirname,
-    alias: {
-      Main: 'app/components/Main.jsx',
-      Nav: 'app/components/Nav.jsx',
-      ListM: 'app/components/ListM.jsx',
-      About: 'app/components/About.jsx'
-    },
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    loaders: [
-      {
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0']
-        },
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/
-      }
-    ]
+  module :{
+    rules:[{
+      use : 'babel-loader',
+      query :{
+        presets:['react','es2015','es2017']
+      },
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/
+    }]
   }
 };
