cd1fcd3f0e6260c66663f8962c2e3a94e822da2a
Felipe <f.alfonso@res-ear.ch>
2018-07-09 02:19:08 -0400

    fixes

--- app/app.jsx
+++ app/app.jsx
@@ -1,35 +1,52 @@
 
-var React = require('react');
-var ReactDOM = require('react-dom');
-// var {Provider} = require('react-redux');
-var redux = require('redux');
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+// // var {Provider} = require('react-redux');
+// var redux = require('redux');
 
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+// var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-// var TodoApp = require('TodoApp');
-// var actions = require('actions');
-// var store = require('configureStore').configure();
-// var TodoAPI = require('TodoAPI');
+// // var TodoApp = require('TodoApp');
+// // var actions = require('actions');
+// // var store = require('configureStore').configure();
+// // var TodoAPI = require('TodoAPI');
 
-store.subscribe(() => {
-	var state = store.getState();
-	console.log('New state', state);
-	TodoAPI.setTodos(state.todos);
-});
+// store.subscribe(() => {
+// 	var state = store.getState();
+// 	console.log('New state', state);
+// 	TodoAPI.setTodos(state.todos);
+// });
 
-var initialTodos = TodoAPI.getTodos();
-store.dispatch(actions.addTodos(initialTodos));
+// var initialTodos = TodoAPI.getTodos();
+// store.dispatch(actions.addTodos(initialTodos));
 
-//Load foundation
-$(document).foundation();
+// //Load foundation
+// $(document).foundation();
 
-//App.scss
-// require('style!css!sass!applicationStyles');
+// //App.scss
+// // require('style!css!sass!applicationStyles');
+
+// ReactDOM.render(
+// 	<Provider store={store}>
+// 		<TodoApp />
+// 	</Provider>,
+// 	document.getElementById('app')
+// );
+
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { Provider } from 'redux';
+import { Router, routes, browserHistory } from 'react-router';
+//import routes from './routes';
+import configureStore from './store/configureStore.jsx';
+
+const store = configureStore();
 
 ReactDOM.render(
-	<Provider store={store}>
-		<TodoApp />
-	</Provider>,
-	document.getElementById('app')
-);
-
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
+, document.getElementById('app'));
---
+++ app/store/configureStore.jsx
@@ -0,0 +1,23 @@
+var redux = require('redux');
+var thunk = require('redux-thunk').default;
+var {nameReducer, hobbiesReducer, moviesReducer, mapReducer} = require('./../reducers/index.jsx');
+
+export var configure = () => {
+  var reducer = redux.combineReducers({
+    name: nameReducer,
+    hobbies: hobbiesReducer,
+    movies: moviesReducer,
+    map: mapReducer
+  });
+
+  // var store = redux.createStore(reducer, redux.compose(
+  //   redux.applyMiddleware(thunk),
+  //   window.devToolsExtension ? window.devToolsExtension() : f => f
+  // ));
+      var store = redux.createStore(reducer, redux.compose(
+        redux.applyMiddleware(thunk),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    ));
+
+  return store;
+}
