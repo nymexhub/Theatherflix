d7284b0d9f1f3453a719f0284f3f20b936972e86
Felipe <f.alfonso@res-ear.ch>
2018-07-31 01:26:22 -0400

    fixes in the app file

--- app/app.jsx
+++ app/app.jsx
@@ -1,5 +1,8 @@
-import React from ('react');
-import ReactDOM from ('react-dom');
+
+
+// import React from ('react');
+// import ReactDOM from ('react-dom');
+
 // var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 // Load foundation
