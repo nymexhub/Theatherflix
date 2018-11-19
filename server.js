e8cb918745d5094d8456350e13c7be5b3772a5c0
Felipe Alfonso Gonzalez <felipe@Felipes-MacBook-Air.local>
2018-11-19 02:29:03 -0300

    new stuff

--- server.js
+++ server.js
@@ -11,20 +11,10 @@
 var express = require('express');
 
 //Create app
-var app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(function(req, res, next){
-	if(req.headers['x-forwarded-proto'] === 'https'){
-		res.redirect('http://' + req.hostname + req.url);
-	} else {
-		next();
-	}
-});
 
 
 var app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 
 
 
