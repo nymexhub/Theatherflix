738597b564cde57010a5b8366b7970fdd2ee22c0
Felipe <f.alfonso@res-ear.ch>
2023-08-01 03:07:38 -0400

    Updates - tree definitions

--- popup/popup.js
+++ popup/popup.js
@@ -1,23 +1,19 @@
+// popup.js
 
-document.addEventListener('DOMContentLoaded', () => {
-  const configButton = document.getElementById('config-button');
-  const configSection = document.getElementById('config-section');
-
-  configButton.addEventListener('click', () => {
-    if (configSection.style.display === 'none') {
-      configSection.style.display = 'block';
-    } else {
-      configSection.style.display = 'none';
-    }
+$(document).ready(function() {
+  // Add accordion behavior using jQuery
+  $('.accordion-header').click(function() {
+    $('.accordion-content').slideToggle();
   });
 
-  const saveButton = document.getElementById('save-button');
-  const apiKeyInput = document.getElementById('api-key');
-
-  saveButton.addEventListener('click', () => {
-    const apiKey = apiKeyInput.value;
+  // Add a click event listener to save the API key
+  $('#save-button').click(function() {
+    const apiKey = $('#api-key').val();
     if (apiKey) {
+      // Save the API key to local storage or wherever you need to store it
+      // For simplicity, let's store it in local storage
       localStorage.setItem('tmdb_api_key', apiKey);
+      // Optionally, you can display a success message to the user
       alert('API Key saved successfully!');
     } else {
       alert('Please enter a valid API Key.');
